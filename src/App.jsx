ead0a8c00ada296088cb592d4cb217ed38ff3dc8
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-05-30 17:02:52 +0700

    feat: update ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="w-full h-screen bg-stone-950 text-white font-mono">
       <Navbar />
-      <div className="mx-5">
-        <div className="pt-20 flex flex-col mb-10 items-center">
+
+      <div className="block md:hidden mx-5">
+        <div className="pt-20 flex flex-col mb-10">
           <h1 className="font-bold text-3xl">Aria Adi Pramesta</h1>
-          <p className="font-semibold text-xs ">
+          <p className="font-semibold text-md ">
             Senior high school graduate in 2023
           </p>
         </div>
@@ -38,8 +39,40 @@ function App() {
         </div>
       </div>
 
+      <div className=" hidden md:block w-[640px] mx-auto pt-36">
+        <div className=" mb-14">
+          <h1 className=" text-6xl text-white font-bold">Aria Adi Pramesta</h1>
+          <p className="font-semibold">High school graduate in 2023</p>
+        </div>
+
+        <div className="flex gap-7">
+          <img
+            src="/assets/profile.jpeg"
+            alt="profile"
+            className="rounded-xl h-96"
+          />
+
+          <div>
+            <h2 className="flex items-end gap-3 mb-2">
+              <img
+                src="/assets/bearmentor-bear.png"
+                alt="bearmentor logo"
+                className="h-10"
+              />
+              <p className="font-semibold text-3xl">Education</p>
+            </h2>
+
+            <p className="opacity-70">
+              Now, I am continuing my education at Bearmentor Bootcamp to
+              increase my knowledge, I am learning about Full Stack Web
+              Development to become a programmer.
+            </p>
+          </div>
+        </div>
+      </div>
+
       <div className="fixed bottom-0 w-screen">
-        <p className="text-center font-semibold text-xs">
+        <p className="text-center font-semibold text-xs sm:text-lg">
           © 2024 Aria Adi Pramesta
         </p>
       </div>
