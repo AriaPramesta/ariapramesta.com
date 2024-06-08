05131746fbc54121937366dab58bca652c9da6a0
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-06-08 15:51:49 +0700

    feat: prepare for section component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Home from "./section-components/home";
 import Education from "./section-components/education";
+import Skills from "./section-components/skills";
+import Projects from "./section-components/projects";
+import Contact from "./section-components/contact";
 
 function App() {
   return (
@@ -9,7 +12,14 @@ function App() {
       <Navbar />
 
       <Home />
+
       <Education />
+
+      <Skills />
+
+      <Projects />
+
+      <Contact />
 
       <div className="bottom-0">
         <p className="text-center font-semibold text-xs sm:text-lg">
