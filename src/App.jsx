490a44db26b7efde9dfbd0faae87d4d358a249ce
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-06-08 15:16:54 +0700

    feat: create education section component and update the app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,17 @@
 import "./App.css";
 import Navbar from "./components/navbar";
+import Home from "./section-components/home";
+import Education from "./section-components/education";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-stone-950 text-white font-mono">
+    <div className="w-full bg-stone-900 text-white font-mono">
       <Navbar />
 
-      <div className="block md:hidden mx-5">
-        <div className="pt-20 flex flex-col mb-10">
-          <h1 className="font-bold text-3xl">Aria Adi Pramesta</h1>
-          <p className="font-semibold text-md ">
-            Senior high school graduate in 2023
-          </p>
-        </div>
-        <div className="flex flex-col gap-10">
-          <img
-            src="/assets/profile.jpeg"
-            alt="profile"
-            className=" mx-28 rounded-xl"
-          />
+      <Home />
+      <Education />
 
-          <div className="flex flex-col">
-            <h2 className="flex items-center gap-2">
-              <img
-                src="/assets/bearmentor-bear.png"
-                alt="bearmentor logo"
-                className="h-5"
-              />
-              <p className="text-md font-semibold">Education</p>
-            </h2>
-
-            <p className="text-xs opacity-75">
-              Now, I am continuing my education at Bearmentor Bootcamp to
-              increase my knowledge, I am learning about Full Stack Web
-              Development to become a programmer.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className=" hidden md:block w-[640px] mx-auto pt-36">
-        <div className=" mb-14">
-          <h1 className=" text-6xl text-white font-bold">Aria Adi Pramesta</h1>
-          <p className="font-semibold">High school graduate in 2023</p>
-        </div>
-
-        <div className="flex gap-7">
-          <img
-            src="/assets/profile.jpeg"
-            alt="profile"
-            className="rounded-xl h-96"
-          />
-
-          <div>
-            <h2 className="flex items-end gap-3 mb-2">
-              <img
-                src="/assets/bearmentor-bear.png"
-                alt="bearmentor logo"
-                className="h-10"
-              />
-              <p className="font-semibold text-3xl">Education</p>
-            </h2>
-
-            <p className="opacity-70">
-              Now, I am continuing my education at Bearmentor Bootcamp to
-              increase my knowledge, I am learning about Full Stack Web
-              Development to become a programmer.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="fixed bottom-0 w-screen">
+      <div className="bottom-0">
         <p className="text-center font-semibold text-xs sm:text-lg">
           © 2024 Aria Adi Pramesta
         </p>
---
+++ src/section-components/education.jsx
@@ -0,0 +1,44 @@
+export default function Education() {
+  return (
+    <div className="w-[640px] mx-auto">
+      <div className="flex gap-3 items-center mb-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+          className="w-10"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+          />
+        </svg>
+
+        <h1 className="font-bold text-3xl">Education</h1>
+      </div>
+
+      <div className="mb-10">
+        <h1 className="font-semibold text-xl mb-5">Self-taught</h1>
+        <p>
+          Since I graduated from high school, I have studied web development
+          self-taught by watching learning videos, such as HTML, CSS, and even
+          JavaScript.
+        </p>
+      </div>
+
+      <div>
+        <h1 className="font-semibold text-xl mb-5">Bearmentor Bootcamp</h1>
+        <p>
+          I joined Bearmentor Bootcamp to further improve my skills, I learned a
+          lot there. starting from Front-End, such as creating a personal
+          website that only uses HTML and CSS, learning JavaScript and learning
+          React. I also studied Back-End, such as creating rest API applications
+        </p>
+      </div>
+    </div>
+  );
+}
