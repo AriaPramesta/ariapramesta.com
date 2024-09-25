fb64fd56221db0146df121a55a775ee07eaf0290
Aria Adi Pramesta <pramestaaria@gmail.com>
2024-09-25 14:03:42 +0700

    feat: fix size

--- src/section-components/home.jsx
+++ src/section-components/home.jsx
@@ -7,12 +7,12 @@ export default function Home() {
       </div>
       <div className="flex flex-row-reverse justify-between gap-5 xl:gap-20">
         <div className="bg-[url('/public/assets/profile.jpeg')] bg-cover w-[150px] min-h-[150px] md:min-w-[350px] md:min-h-[200px] xl:w-[600px] xl:h-[500px]"></div>
-        <div className="max-w-[150px] md:max-w-[300px] lg:max-w-[400px] xl:max-w-[700px]">
-          <div className="flex flex-col gap-5 md:gap-20 xl:gap-32">
+        <div className="max-w-[175px] md:max-w-[300px] lg:max-w-[400px] xl:max-w-[700px]">
+          <div className="flex flex-col gap-4 md:gap-20 xl:gap-32">
             <h1 className="text-[10px] md:text-right md:text-xl mt-5">
               The Dreamseeker
             </h1>
-            <div className="flex flex-col gap-5 md:gap-10 text-[5px] md:text-xs xl:text-base">
+            <div className="flex flex-col gap-3 md:gap-10 text-[6px] md:text-xs xl:text-base">
               <p>
                 I am a high school graduate, since graduating in 2023 I have
                 learned a lot about web development, starting from self-taught
